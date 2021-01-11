cd8c20f8af20ae1436408baf5f666c29cd17dec6
wutango <nickkdb@gmail.com>
2021-01-11 15:45:27 -0500

    required path

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express= require("express");
+var path= require("path");
 
 let PORT= process.env.PORT || 8080;
 
 let app= express();
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
