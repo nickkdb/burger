da2273a8b88c91634ffc4d2e661b7ce36b865c33
wutango <nickkdb@gmail.com>
2021-01-11 15:23:52 -0500

    added JAWSDB to app for heroku functionality

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 let sql= require("mysql");
 
-var connection= sql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = sql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = sql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
     database: "burgers_db"
 });
+}
 
 connection.connect(err => {
     if (err) {
